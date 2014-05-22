c36d7dccebd7673a95dbb7d3c50f440b1999e5a8
Oleg Galbert <ogcraft@gmail.com>
2014-05-22 13:59:57 +0300

    ...

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject mooviefishsrv "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject mooviefishsrv "0.1.1-alpha"
+  :description "MoovieFish site backend"
   :url "http://example.com/FIXME"
+  ;; CLJ AND CLJS source code path
+  :source-paths ["src" "src-cljs"]
+  
   :dependencies [[org.clojure/clojure "1.6.0"]
   [compojure "1.1.6"]
   [hiccup "1.0.5"]
@@ -8,8 +11,11 @@
   [cheshire "5.3.1"]
   [lib-noir "0.8.2"]
   [com.ashafa/clutch "0.4.0-RC1"]
-  [ring-server "0.3.1"]]
-  :plugins [[lein-ring "0.8.10"]]
+  [ring-server "0.3.1"]
+  [org.clojure/clojurescript "0.0-2197"]]
+
+  :plugins [[lein-ring "0.8.10"] 
+            [lein-cljsbuild "1.0.3"]]
   :ring {:handler mooviefishsrv.handler/app
     :init mooviefishsrv.handler/init
     :destroy mooviefishsrv.handler/destroy}
@@ -19,4 +25,15 @@
       {:ring
       {:open-browser? false, :stacktraces? false, :auto-reload? false}}
         :dev
-      {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.2.1"]]}})
+      {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.2.1"]]}}
+  ;; cljsbuild options configuration
+  :cljsbuild {:builds
+              [{;; CLJS source code path
+                :source-paths ["src-cljs"]
+                ;; Google Closure (CLS) options configuration
+                :compiler {;; CLS generated JS script filename
+                           :output-to "resources/public/js/mooviefishsrv.js"
+                           ;; minimal JS optimization directive
+                           :optimizations :whitespace
+                           ;; generated JS code prettyfication
+                           :pretty-print true}}]})
