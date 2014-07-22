b13c94471f9187cd5d5fa21dba047c9263e400b5
ogcraft <ogcraft@gmail.com>
2014-07-22 18:44:02 +0300

    ...

--- src/mooviefishsrv/tools/sitegen.clj
+++ src/mooviefishsrv/tools/sitegen.clj
@@ -9,13 +9,10 @@
 		[clj-time.format :as time-format]
 		[net.cgrand.enlive-html :as html]))
 
-(def index-template 	{:ru "site-template/ru/index.selmer.html" })
 (def cinema-template  	{:ru "site-template/ru/cinema.selmer.html"})
 
 (defn file-exists? [fn] 
 	(.exists (clojure.java.io/as-file fn)))
-
-;(def cinema-page-ru (slurp cinema-html-ru))
 
 (defn generate-cinema-page [lang]
 	(let [	active-movies 	(db/get-movies-active lang)
@@ -24,37 +21,10 @@
 		(selmer.parser/render p 
 			{:active-movies active-movies :new-movies new-movies})))
 
-(defn ttt []
+(defn write-cinema-page [lang path]
+	(spit (str path "/cinema.html") (generate-cinema-page "ru")))
+
+(defn load-movies-and-generate-cinema [lang path]
 	(db/load-movies-data)
-
-	;(prn "Movies: " (count @db/movies))
-	;(let [  rr (html/html-resource (java.io.StringReader. (slurp cinema-html)))
-	;		current-movies (html/select rr [:ul.left-movies :div.movie-entry])
-	;		new-movies (html/select rr [:ul.right-movies :div.movie-entry])]
-	;	(prn "current-movies: " (count current-movies))
-	;	(prn "new-movies: " (count new-movies))
-	;)
-	
-	;(html/defsnippet 
-	;	movie-active-snippet (java.io.StringReader. (slurp cinema-html))
-	;	{[[:ul.left-movies] [:li]]}
-	;	[active-movies]
-	;	[:div] (html/content "aaaaaa"))
-	;	[:span.author] (html/content (:author post))
-	;	[:div.post-body] (html/content (:body post)))
-
-	;(html/deftemplate 
-	;	cinema-template (java.io.StringReader. (slurp cinema-html))
-	;	[active-movies new-movies]
-	;	[:ul.left-movies] (html/content "<p>current movies</p>") 
-	;	[:ul.right-movies] (html/content "<p>new movies</p>"))
-
-	;(apply str (cinema-template (db/get-movies-active "en") (db/get-movies-new "en")))
-	;(cinema-template (db/get-movies-active "en") (db/get-movies-new "en"))
-	;(html/transform cinema-template [:li.movie-entry]  
-    ;    (html/clone-for [i (range 4)] identity))
-	;(prn cinema-page)
-	;(pprint (first (db/get-movies-new "ru")))
-	(generate-cinema-page "ru")
-)
+	(write-cinema-page lang path))
 
