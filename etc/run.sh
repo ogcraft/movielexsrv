cd677e9e4ccd80bed82ab9c5c18efc11f7627472
ogcraft <ogcraft@gmail.com>
2014-07-23 06:29:28 -0400

    ..

--- etc/run.sh
+++ etc/run.sh
@@ -2,7 +2,9 @@
  
 MY_APP_HOME="/var/www/mooviefish.com/app"
 MY_APP_JAR="mooviefishsrv-0.1.2-beta-standalone.jar"
- 
-java -Xms256m -Xmx512m -jar $MY_APP_HOME/$MY_APP_JAR &
+MY_LOG=$MY_APP_HOME/log/mooviefishsrv.log
+
+java -Xms256m -Xmx512m -jar $MY_APP_HOME/$MY_APP_JAR > $MY_LOG &
 echo $! > $MY_APP_HOME/tmp/pids/mooviefishsrv.pid
+
 exit 0
