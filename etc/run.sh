b219300f31fd7a6f01d74c5cd9c08bf999ccca39
ogcraft <ogcraft@gmail.com>
2014-08-02 08:10:47 -0400

    ...

--- etc/run.sh
+++ etc/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
  
-MY_APP_HOME="/var/www/mooviefish.com/app"
-MY_APP_JAR="mooviefishsrv-0.1.2-beta-standalone.jar"
-MY_LOG=$MY_APP_HOME/log/mooviefishsrv.log
+MY_APP_HOME="/var/www/movielex.com/app"
+MY_APP_JAR="movielexsrv-0.1.2-beta-standalone.jar"
+MY_LOG=$MY_APP_HOME/log/movielexsrv.log
 
 java -Xms256m -Xmx512m -jar $MY_APP_HOME/$MY_APP_JAR > $MY_LOG &
-echo $! > $MY_APP_HOME/tmp/pids/mooviefishsrv.pid
+echo $! > $MY_APP_HOME/tmp/pids/movielexsrv.pid
 
 exit 0
