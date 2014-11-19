8c0f66fc8e07f8fcae74f452f51e0918294f70fa
Oleg Galbert <ogcraft@gmail.com>
2014-11-19 17:01:54 +0200

    ...

--- scripts/mv-get-image.sh
+++ scripts/mv-get-image.sh
@@ -6,7 +6,7 @@ image_fname=${url##*/}.jpg
 if [[ $url == *imdb.com* ]]
 then
 	echo "Downloading from $1 -> $image_fname"
-	image_url=`curl -s www.imdb.com/title/tt0455944/ | grep image_src | grep -o -E 'href="([^"#]+)"' | cut -d'"' -f2`
+	image_url=`curl -s ${url}/ | grep image_src | grep -o -E 'href="([^"#]+)"' | cut -d'"' -f2`
 	curl -L -o $image_fname $image_url
 fi
 
