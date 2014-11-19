e5382bc7271f96770640ca38f19f263190eda084
Oleg Galbert <ogcraft@gmail.com>
2014-11-19 17:00:48 +0200

    ...

---
+++ scripts/mv-get-image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#set -x
+url=${1%/}
+image_fname=${url##*/}.jpg
+
+if [[ $url == *imdb.com* ]]
+then
+	echo "Downloading from $1 -> $image_fname"
+	image_url=`curl -s www.imdb.com/title/tt0455944/ | grep image_src | grep -o -E 'href="([^"#]+)"' | cut -d'"' -f2`
+	curl -L -o $image_fname $image_url
+fi
+
+
+if [[ $url == *kinopoisk.ru* ]]
+then
+	echo "Downloading from $1 -> $image_fname"
+	image_url="http://www.kinopoisk.ru/images/film_big/${image_fname}"
+	curl -L -o $image_fname $image_url
+fi
+
+
+exit 0;
