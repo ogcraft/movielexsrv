11ae8fa9b40363b6080e4e3fc170a6455297f442
ogcraft <ogcraft@gmail.com>
2015-01-17 18:48:16 +0200

    ...

--- scripts/mvemailer.py
+++ scripts/mvemailer.py
@@ -1,82 +1,92 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*- 
 
+import sys
+import time
 import smtplib
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
-fromaddr = 'ogcraft@gmail.com'
-toaddrs  = 'olegg@waves.com'
+emails_file = "data/emails.clj"
 
+def read_emails(fl):
+    return open(fl).read().strip()[1:-1].split()
 
-# Create message container - the correct MIME type is multipart/alternative.
-msg = MIMEMultipart('alternative')
-msg['Subject'] = "Link"
-msg['From'] = fromaddr
-msg['To'] = toaddrs
+email_addresses = read_emails(emails_file)
+#email_addresses = ["ogcraft@gmail.com", "aibolit6661@gmail.com"]
+
+print "Loaded ", len(email_addresses), " emails"
+
+#sys.exit(0)
+
+fromaddr = 'movielex.com@gmail.com'
+toaddrs  = 'ogcraft@gmail.com'
 
 # Create the body of the message (a plain-text and an HTML version).
-text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttps://www.python.org"
+text = "Дорогие друзья,\n с большим опозданием, но все таки появилась русская озвучка к фильму \"Хоббит. битва пяти воинств\".\n Все кто не успел посмотреть фильм в кинотеатре без перевода (или в интернете с переводом) успевайте.\n Загляните на наш сайт www.movielex.com, установите приложение movielex и бежим в кино!\nВаш movielex.com"
+
 html = """\
 <html>
   <head></head>
   <body>
-    <p>Hi!<br>
-       How are you?<br>
-       Here is the <a href="https://www.python.org">link</a> you wanted.
+    <p>
+    Дорогие друзья,<br>
+    с большим опозданием, но все таки появилась русская озвучка к фильму «Хоббит - битва пяти воинств».<br> 
+    Все кто не успел посмотреть фильм в кинотеатре без перевода (или в интернете с переводом) успевайте.<br> Загляните на наш сайт
+     <a href="http://www.movielex.com">www.movielex.com</a>, установите приложение movielex и бежим в кино!"
     </p>
-  </body>
+    <p>
+    Ваш movielex.com
+    </p>
+   </body>
 </html>
 """
 
-# Record the MIME types of both parts - text/plain and text/html.
-part1 = MIMEText(text, 'plain')
-part2 = MIMEText(html, 'html')
 
-# Attach parts into message container.
-# According to RFC 2046, the last part of a multipart message, in this case
-# the HTML message, is best and preferred.
-msg.attach(part1)
-msg.attach(part2)
-
-# Send the message via local SMTP server.
-# Credentials (if needed)
-username = ''
-password = ''
-
-# The actual mail send
-server = smtplib.SMTP_SSL('smtp.gmail.com:465')
-#server = smtplib.SMTP('smtp.gmail.com:587')
-#server.starttls()
-
-# sendmail function takes 3 arguments: sender's address, recipient's address
-# and message to send - here it is sent as one string.
-
-server.login(username,password)
-server.sendmail(fromaddr, toaddrs, msg.as_string())
-#server.sendmail(fromaddr, toaddrs, msg)
-server.quit()
+def send_msg(fromaddr, toaddrs, text, html):
+    print "Sending email to :", toaddrs 
+    # Create message container - the correct MIME type is multipart/alternative.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "У MovieLex появилась русская озвучка к фильму «Хоббит - битва пяти воинств»"
+    msg['From'] = fromaddr
+    msg['To'] = toaddrs
 
 
+    # Record the MIME types of both parts - text/plain and text/html.
+    part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(html, 'html')
 
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in this case
+    # the HTML message, is best and preferred.
+    msg.attach(part1)
+    msg.attach(part2)
 
-# import smtplib
+    # Send the message via local SMTP server.
+    # Credentials (if needed)
+    username = 'ogcraft@gmail.com'
+    password = 'c20h25n3o'
 
-# fromaddr = 'ogcraft@gmail.com'
-# toaddrs  = 'olegg@waves.com'
-# msg = 'There a test message!'
+    # The actual mail send
+    server = smtplib.SMTP_SSL('smtp.gmail.com:465')
+    #server = smtplib.SMTP('smtp.gmail.com:587')
+    #server.starttls()
 
+    # sendmail function takes 3 arguments: sender's address, recipient's address
+    # and message to send - here it is sent as one string.
 
-# # Credentials (if needed)
-# username = 'ogcraft'
-# password = 'c20h25n3o'
+    server.login(username,password)
+    server.sendmail(fromaddr, toaddrs, msg.as_string())
+    #server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
 
-# # The actual mail send
-# server = smtplib.SMTP_SSL('smtp.gmail.com:465')
-# #server = smtplib.SMTP('smtp.gmail.com:587')
-# #server.starttls()
-# server.login(username,password)
-# server.sendmail(fromaddr, toaddrs, msg)
-# server.quit()
+count = 0
+for a in email_addresses:
+    count = count + 1
+    print "----------", count, " of ", len(email_addresses), "----------"
+    send_msg(fromaddr, a, text, html)
+    time.sleep(2)
+
 
